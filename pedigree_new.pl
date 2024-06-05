8f561f08b16b5ad6621fe56c32f914d63cd6666c
1993zsj123 <46735843+1993zsj123@users.noreply.github.com>
2024-06-05 16:12:45 +0800

    Add files via upload

---
+++ pedigree_new.pl
@@ -0,0 +1,178 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+my $path=qx(pwd);
+my $sample = shift;
+
+open IN, "<", $sample  or die $!;
+my @list;
+while(<IN>){
+	chomp;
+	my ($pedigree, $proband, $breed,$sex, $dam, $sire) = split /\s+/;
+	open GVCFLIST, ">/xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.gvcf.list";
+	print GVCFLIST "/xtdisk/wanggd_kiz/zhangsj/pedigree/data/gvcf/$proband.rawvariant.g.vcf.gz
+/xtdisk/wanggd_kiz/zhangsj/pedigree/data/gvcf/$dam.rawvariant.g.vcf.gz
+/xtdisk/wanggd_kiz/zhangsj/pedigree/data/gvcf/$sire.rawvariant.g.vcf.gz
+";
+	open DGPED, ">/xtdisk/wanggd_kiz/zhangsj/pedigree/work/ped/$pedigree.dg.ped";
+	print DGPED "$pedigree\t$proband\t$dam\t$sire\t$sex\t0";
+	open TDPED, ">/xtdisk/wanggd_kiz/zhangsj/pedigree/work/ped/$pedigree.td.ped";
+	print TDPED "$pedigree\t$dam\t0\t0\t1
+$pedigree\t$sire\t0\t0\t2
+$pedigree\t$proband\t$dam\t$sire\t$sex
+";
+	open PEDIGREE, ">/xtdisk/wanggd_kiz/zhangsj/pedigree/work/script/$pedigree.sh";
+	print PEDIGREE "#!/bin/bash
+
+#PBS -N $pedigree.sh
+#PBS -o $pedigree.sh.stdout
+#PBS -e $pedigree.sh.stderr
+#PBS -q core40
+#PBS -l mem=16gb,walltime=572:00:00,nodes=1:ppn=4
+#HSCHED -s hschedd
+#PPN limit 4
+# Description:
+set -e
+set -u
+
+WORK=/xtdisk/wanggd_kiz/zhangsj/pedigree/work
+REF=/xtdisk/wanggd_kiz/zhangsj/ref/Canis_familiaris.CanFam3.1.dna.toplevel.fa
+KNOWSITE=/xtdisk/wanggd_kiz/zhangsj/ref/58indiv.unifiedgenotyper.recalibrated_95.5_filtered.pass_snp.vcf
+SAMTOOLS=/xtdisk/wanggd_kiz/zhangsj/bin/anaconda3/bin/samtools
+PICARD_DIR=/xtdisk/wanggd_kiz/zhangsj/bin/software/picard-tools-1.96
+GATK=/xtdisk/wanggd_kiz/zhangsj/bin/software/GenomeAnalysisTK.jar
+VCFTOOLS=/xtdisk/wanggd_kiz/zhangsj/bin/anaconda3/bin/vcftools
+BCFTOOLS=/xtdisk/wanggd_kiz/zhangsj/bin/software/smcpp/bin/bcftools
+CHRBED=/xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/intervals.list
+DENOVOGEAR=/xtdisk/wanggd_kiz/zhangsj/bin/software/denovogear-0.5.4-Linux-x86_64/bin/denovogear
+TRIODENOVO=/xtdisk/wanggd_kiz/zhangsj/bin/software/triodenovo.0.06/bin/triodenovo
+FILTERE=/xtdisk/wanggd_kiz/zhangsj/bin/filtere.7.pl
+
+
+
+cd $path
+
+#VCF Calling
+
+##First step 
+java -Xmx16g -Xms16g -Djava.io.tmpdir=1rst/temp \\
+-jar \$GATK \\
+-T CombineGVCFs \\
+-R \$REF \\
+-L \$CHRBED \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.gvcf.list \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.g.vcf.gz
+ 
+
+##Second step
+java -Xmx16g -Xms16g -Djava.io.tmpdir=1rst/temp \\
+-jar \$GATK \\
+-T GenotypeGVCFs \\
+-nt 4 \\
+-R \$REF \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.g.vcf.gz \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.raw.vcf
+
+#VCF Filter
+
+bgzip -f /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.raw.vcf
+tabix -p vcf /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.raw.vcf.gz
+
+java -Xmx16g -Xms16g -Djava.io.tmpdir=1rst/temp \\
+-jar \$GATK \\
+-T SelectVariants \\
+-R \$REF \\
+-L \$CHRBED \\
+-selectType  SNP \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.raw.vcf.gz \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.snp.raw.vcf.gz
+
+
+java -Xmx16g -Xms16g -Djava.io.tmpdir=1rst/temp \\
+-jar \$GATK \\
+-T SelectVariants \\
+-R \$REF \\
+-L \$CHRBED \\
+-selectType  INDEL \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.raw.vcf.gz \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.indel.raw.vcf.gz
+
+
+java -Xmx16g -Xms16g -Djava.io.tmpdir=1rst/temp \\
+-jar \$GATK \\
+-T  VariantFiltration \\
+-R \$REF \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.indel.raw.vcf.gz \\
+--filterExpression \"QD \< 2.0 || FS \> 200.0 || SOR \> 10.0 || InbreedingCoeff<-0.8\" \\
+--filterName  \"Filter\" \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.indel.filter.vcf.gz
+
+java -Xmx16g -Xms16g -Djava.io.tmpdir=1rst/temp \\
+-jar \$GATK \\
+-T VariantFiltration \\
+-R \$REF \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.snp.raw.vcf.gz \\
+--filterExpression \"QD \< 2.0 || FS \> 60.0 || MQ \< 40.0 || QUAL \< 50.0 || SOR \> 3.0 || MQRankSum \< -12.5\" \\
+--filterName \"Filter\" \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.snp.filter.vcf.gz
+
+gatk \\
+MergeVcfs \\
+-I /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.indel.filter.vcf.gz \\
+-I /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.snp.filter.vcf.gz \\
+-O /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.all.filter.vcf.gz
+
+java -Xmx16g -Xms16g -Djava.io.tmpdir=1rst/temp \\
+-jar \$GATK \\
+-T SelectVariants \\
+-R \$REF \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.all.filter.vcf.gz \\
+-ef -o /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.all.filtered.vcf.gz
+
+#rm
+rm /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.g.vcf.gz
+rm /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree\_all.raw.vcf.gz
+rm /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.snp.raw.vcf.gz
+rm /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.indel.raw.vcf.gz
+rm /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.snp.filter.vcf.gz 
+rm /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.indel.filter.vcf.gz 
+rm /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.all.filter.vcf.gz
+
+#DNM
+mkdir -p /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree
+mkdir -p /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk
+mkdir -p /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/overlap
+
+##GATK
+
+java -Xmx8g  -jar \$GATK -R \$REF -T SelectVariants \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.all.filtered.vcf.gz \\
+-sn $proband -env \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/son_$proband.vcf
+
+java -Xmx8g  -jar \$GATK -R \$REF -T SelectVariants \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.all.filtered.vcf.gz \\
+-sn $dam -sn $sire -env \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/parent_$dam\_$sire.vcf
+
+java -Xmx8g  -jar \$GATK -R \$REF -T SelectVariants \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/son_$proband.vcf \\
+--discordance /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/parent_$dam\_$sire.vcf \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/denovo_$pedigree.vcf
+
+java -Xmx8g  -jar \$GATK -R \$REF -T SelectVariants \\
+-V /xtdisk/wanggd_kiz/zhangsj/pedigree/data/vcf/$pedigree.all.filtered.vcf.gz \\
+--concordance /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/denovo_$pedigree.vcf \\
+-o /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/fam-denovo_$pedigree.vcf
+
+\$VCFTOOLS --vcf  /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/fam-denovo_$pedigree.vcf \\
+--min-meanDP 12 --max-missing 1 --remove-indels --max-alleles 2 --recode \\
+--out /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/trio_$pedigree.filtered
+
+\$BCFTOOLS  view  -s $dam,$sire,$proband \\
+/xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/trio_$pedigree.filtered.recode.vcf > /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/trio_$pedigree.filtered.vcf
+
+perl \$FILTERE /xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/gatk/trio_$pedigree.filtered.vcf >/xtdisk/wanggd_kiz/zhangsj/pedigree/work/$pedigree/overlap/$pedigree\_overlap.dnm.vcf
+";
+}
